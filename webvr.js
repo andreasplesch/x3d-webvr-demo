99a55b1a5db4a7177beff7c3c20083c2657042e3
Andreas Plesch <andreasplesch@users.noreply.github.com>
2016-12-25 11:47:44 -0500

    button default style div

--- webvr.js
+++ webvr.js
@@ -161,7 +161,25 @@ function init() {
   // inject enter/exit VR button
   var enterVRBtn = document.createElement('button');
   enterVRBtn.setAttribute('id', 'enter-vr-btn');
-  document.body.appendChild(enterVRBtn);
+  //wrap in div for default style
+  var enterVRDiv = document.createElement('div');
+  enterVRDiv.setAttribute('id', 'enter-vr-div');
+  enterVRDiv.style = '
+    background: rgba(0, 0, 0, .35) url("icon-goggles-white.svg") 50% 50% no-repeat;
+    background-size: 70% 70%;
+    color: #fff;
+    height: 50px;
+    width: 60px;
+    border: none;
+    background-color: rgba(120, 120, 120, .35);
+    padding: 1px 6px;
+    position:absolute;
+    top:10px;
+    left:10px;
+    cursor: pointer;
+  ';
+  enterVRDiv.appendChild(enterVRBtn);
+  document.body.appendChild(enterVRDiv);
   enterVRBtn.addEventListener('click', function(event){
     enterVR();
   });
